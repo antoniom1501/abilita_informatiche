c826a9af63a6b9ab3708ce5a3bb2c4581c377835
Antonio Mastropasqua <95907907+antoniom1501@users.noreply.github.com>
2021-12-10 11:44:25 +0000

    main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,33 @@
 import math
 import cmath
-a=int(input("a="))
-b=int(input("b="))
-print("Somma=",a+b)
 
-def perimetro(r)
+#---------------------------------------
+a=complex(input("a="))
+b=complex(input("b="))
+print("Somma=",type(a+b),a+b)
+#---------------------------------------
 r=float(input("Raggio="))
-per=2*math.pi*r
-print("Perimetro=",per)
+def perimetrocir(r):
+  per=2*math.pi*r
+  print("Perimetro=",per)
+perimetrocir(r)
+#---------------------------------------
+print(math.tan(math.pi/4))
+print(math.cos(math.pi))
+#---------------------------------------
+c1=True
+c2=False
+
+if(c1 and not c2):
+  print("c1 and not c2 = True, so b^2=",b**2)
+#---------------------------------------
+p1="ciao, "
+p2="come va?"
+print(p1+p2)
+#---------------------------------------
+vec=[1,2,3]
+if(2 in vec):
+  print("yes")
+else:
+  print("no")
+  
