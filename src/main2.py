fabb2ecc400fd967fe5ae9c1cc0ac9b7632080c9
Antonio Mastropasqua <95907907+antoniom1501@users.noreply.github.com>
2021-12-17 08:49:46 +0000

    main2.py

---
+++ src/main2.py
@@ -0,0 +1,16 @@
+l=[]
+print(l)
+a=[1,2,'ciao']
+print(a[0],a[2])
+print('Lunghezza=',len(a))
+b=[3,4]
+print(a+b)
+print(['Hi!']*4)
+print(3 in [1,2,3])
+for x in [1,2,3]: print(x)
+list=[0,1,2,3,4,5,6,7]
+print(list[0:6])
+print(list[1:5:2])
+print(list[1::2])
+print(list[::2])
+print(list[6:0:-2])
